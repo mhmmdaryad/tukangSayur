6cebca04d8731e84762fcc190133518a12ca9a88
Arya Dewangga <aryadewangga08@gmail.com>
2019-05-18 15:56:46 +0700

    push

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -5,6 +5,5 @@ import halamanAwal.halamanAwal;
 public class driver {
     public static void main(String[] args) {
         new halamanAwal().pilihan();
-        System.out.println("test");
     }
 }
