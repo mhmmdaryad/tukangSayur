32f228b68e930b6cbacb303dd08e127f4c54c8d6
ridharizkiaputri <48399924+ridharizkiaputri@users.noreply.github.com>
2019-05-24 15:00:32 +0800

    pute

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -5,10 +5,8 @@ import halamanAwal.halamanAwal;
 public class driver {
     public static void main(String[] args) {
         new halamanAwal().pilihan();
-<<<<<<< HEAD
+
         System.out.println("test");
-//wien
-=======
->>>>>>> 044bb0bc48c17b53af2cb2ab6f073f026543f363
+
     }
 }
