a5152be7f3fbbb0ba4b5b285ef8e9aad4a7f6e11
wiendewani <wiennuruldewani@gmail.com>
2019-05-24 14:37:00 +0700

    ini wien

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -6,11 +6,8 @@ import java.util.Scanner;
 
 public class driver {
     public static void main(String[] args) {
-        new halamanAwal().pilihan();
-        System.out.println("merge");
-       
-        Scanner s = new (System.in);
-        //a
+      //iniwien
+        System.out.println("Wien");
 
     }
 }
