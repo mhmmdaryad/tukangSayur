581b77ddb311eca1fb7babeea07f19580cc2e2e0
Arya Dewangga <aryadewangga08@gmail.com>
2019-05-24 14:25:27 +0700

    mm

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -8,21 +8,8 @@ public class driver {
     public static void main(String[] args) {
         new halamanAwal().pilihan();
 
-        System.out.println("test");
-<<<<<<< HEAD
-
-        System.out.println();
-
-        System.out.println("oyi");
-        System.out.println();
-        System.out.println();
-        //yaaaa
-
-=======
-        System.out.println("oyi");
-        System.out.println();
-        System.out.println();
+       
         Scanner s = new (System.in);
->>>>>>> 5f239773bdeec79dc08990d8d7ef1ee41c0dccc3
+
     }
 }
