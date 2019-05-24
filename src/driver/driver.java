441dd60ee7570db9e45c3afa426eeb0a9b76723c
wiendewani <wiennuruldewani@gmail.com>
2019-05-24 14:26:24 +0700

    coba

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class driver {
     public static void main(String[] args) {
         new halamanAwal().pilihan();
-
+        System.out.println("merge");
        
         Scanner s = new (System.in);
 
