bc2decb0871c0d4a818b442ee4b9469243c879e9
wiendewani <wiennuruldewani@gmail.com>
2019-05-24 14:16:57 +0700

    //test

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -7,14 +7,13 @@ public class driver {
         new halamanAwal().pilihan();
 
         System.out.println("test");
-<<<<<<< HEAD
 
         System.out.println();
 
-=======
         System.out.println("oyi");
         System.out.println();
         System.out.println();
->>>>>>> 2f16b3eb297b128095466fe18f5d00d9a713fcee
+        //ya
+
     }
 }
