2f16b3eb297b128095466fe18f5d00d9a713fcee
Arya Dewangga <aryadewangga08@gmail.com>
2019-05-24 14:07:09 +0700

    ii

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -7,12 +7,8 @@ public class driver {
         new halamanAwal().pilihan();
 
         System.out.println("test");
-<<<<<<< HEAD
         System.out.println("oyi");
         System.out.println();
         System.out.println();
-=======
-
->>>>>>> 32f228b68e930b6cbacb303dd08e127f4c54c8d6
     }
 }
