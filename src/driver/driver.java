99fed2ff320b7eb3fc50942135a424db135e3729
Arya Dewangga <aryadewangga08@gmail.com>
2019-05-29 16:25:34 +0700

    tampilan awal

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -6,12 +6,6 @@ import java.util.Scanner;
 
 public class driver {
     public static void main(String[] args) {
-        String ojek;
-
-        new halamanAwal().pilihan();
-        System.out.println("awang");
-        Scanner s = new (System.in);
-        System.out.println("kk");
 
     }
 
