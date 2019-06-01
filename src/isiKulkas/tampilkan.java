ca861c40675b6299682ce3cdb456d2d296dbeffd
RizkiAdeSanjaya <49983613+RizkiAdeSanjaya@users.noreply.github.com>
2019-06-01 13:47:22 +0700

    SetGet

--- src/isiKulkas/tampilkan.java
+++ src/isiKulkas/tampilkan.java
@@ -2,11 +2,29 @@ package isiKulkas;
 import java.util.Scanner;
 
 public class tampilkan {
-    public void isiKulkas(){
-        int stock;
-        String nama;
 
+    private int stock;
+    private String nama;
+    public void isiKulkas(){
         Scanner t = new Scanner(System.in);
+
+        public void setStock(int stock){
+            this.stock = stock;
+        }
+
+        public void setNama(String nama){
+            this.nama = nama;
+        }
+
+        public int getStock(){
+            return this.stock;
+        }
+
+        public String GetNama(){
+            return this.nama;
+        }
+
         System.out.println("1. Ikan");
+        System.out.println("2. ");
     }
 }
