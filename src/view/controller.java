a9b970dc2163e818376adedc5925d66a9efc75a6
Arya Dewangga <aryadewangga08@gmail.com>
2019-06-02 11:21:38 +0700

    perombakan

--- src/view/controller.java
+++ src/view/controller.java
@@ -3,18 +3,13 @@ package view;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.lang.Object;
-
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,11 +18,10 @@ import java.util.logging.Logger;
 
 public class controller implements Initializable {
 
-<<<<<<< HEAD
-=======
+
     @FXML
     private Button tombolDaftarPembeli;
->>>>>>> ca861c40675b6299682ce3cdb456d2d296dbeffd
+
     @FXML
     private Button closeButton;
 
@@ -41,7 +35,7 @@ public class controller implements Initializable {
     @FXML
     private Pane paneUtama;
 
-    private utama utama;
+    private driver utama;
 
 
     @Override
