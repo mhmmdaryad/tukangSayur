1be3442d36aac808c9cc185998027cb7de626a50
Arya Dewangga <aryadewangga08@gmail.com>
2019-06-18 20:10:39 +0700

    revisi

---
+++ src/Model/penjual.java
@@ -0,0 +1,32 @@
+package Model;
+
+public class penjual {
+
+    private String id_penjual;
+    private String nama_penjual;
+    private int nohp_penjual;
+
+    public String getId_penjual() {
+        return id_penjual;
+    }
+
+    public void setId_penjual(String id_penjual) {
+        this.id_penjual = id_penjual;
+    }
+
+    public String getNama_penjual() {
+        return nama_penjual;
+    }
+
+    public void setNama_penjual(String nama_penjual) {
+        this.nama_penjual = nama_penjual;
+    }
+
+    public int getNohp_penjual() {
+        return nohp_penjual;
+    }
+
+    public void setNohp_penjual(int nohp_penjual) {
+        this.nohp_penjual = nohp_penjual;
+    }
+}
---
+++ src/isiKulkas/stok.java
@@ -0,0 +1,24 @@
+package isiKulkas;
+
+public class stok {
+
+    private int stock;
+    private String nama;
+
+
+        public void setStock(int stock){
+            this.stock = stock;
+        }
+
+        public void setNama(String nama){
+            this.nama = nama;
+        }
+
+        public int getStock(){
+            return stock;
+        }
+
+        public String getNama(){
+            return nama;
+        }
+}
