175a215bcac06a15f86cb8ff8318716d2f410728
wiendewani <wiennuruldewani@gmail.com>
2019-06-19 19:47:22 +0700

    membuat class user_model

--- TukangsayurNew/src/Model/user_model.java
+++ TukangsayurNew/src/Model/user_model.java
@@ -11,5 +11,24 @@ package Model;
  */
 public class user_model {
     
+    String id_user;
+    String nama;
+
+    public String getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
     
 }
