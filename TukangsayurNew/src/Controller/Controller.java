c6dc4132a70e50d90d5dc96b3586a80d293bf2a1
wiendewani <48399925+wiendewani@users.noreply.github.com>
2019-06-20 22:13:25 +0700

    change controller

--- TukangsayurNew/src/Controller/Controller.java
+++ TukangsayurNew/src/Controller/Controller.java
@@ -10,12 +10,14 @@ package Controller;
  * @author Wien Nurul Dewani
  */
 public class Controller {
-
+import view.*;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        halamanAwal obj = new halamanAwal();
+        obj.setVisible(true);
     }
     
 }
