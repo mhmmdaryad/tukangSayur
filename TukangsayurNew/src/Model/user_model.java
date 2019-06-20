e2306aa099a272949061d257e1faa26e9fa6436b
ridharizkiaputri <48399924+ridharizkiaputri@users.noreply.github.com>
2019-06-20 14:43:51 +0800

    menyimpan inputan textfield ke dalam set get user

--- TukangsayurNew/src/Model/user_model.java
+++ TukangsayurNew/src/Model/user_model.java
@@ -11,10 +11,33 @@ package Model;
  */
 public class user_model {
     
-    String id_user;
+    String username;
     String nama;
     String nohp;
     String alamat;
+    String email;
+    String ttl;
+    String jeniskelamin;
+    String provinsi;
+    String kota;
+    String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
 
     public String getNohp() {
         return nohp;
@@ -32,23 +55,56 @@ public class user_model {
         this.alamat = alamat;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(String ttl) {
+        this.ttl = ttl;
+    }
+
+    public String getJeniskelamin() {
+        return jeniskelamin;
+    }
+
+    public void setJeniskelamin(String jeniskelamin) {
+        this.jeniskelamin = jeniskelamin;
+    }
+
+    public String getProvinsi() {
+        return provinsi;
+    }
+
+    public void setProvinsi(String provinsi) {
+        this.provinsi = provinsi;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
-
-    public String getId_user() {
-        return id_user;
-    }
-
-    public void setId_user(String id_user) {
-        this.id_user = id_user;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
+   
+    
     
     
 }
